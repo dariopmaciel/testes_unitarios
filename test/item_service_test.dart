1207aad262ce3eaf2a66af9f4ddcf04a8dd632b1
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-10-07 09:57:59 -0300

    add mocktail para impedir verbose de teste

--- test/item_service_test.dart
+++ test/item_service_test.dart
@@ -6,11 +6,13 @@ import 'package:testes_unitarios/item_service.dart';
 class ItemRepositoryFake implements IItemRepository {
   @override
   Future<List<Item>> buscarTodosAsync() async {
+    // throw Exception();
     return <Item>[];
   }
 
   @override
   List<Item> buscarTodosSync() {
+    
     return [];
   }
 
@@ -20,12 +22,41 @@ class ItemRepositoryFake implements IItemRepository {
   }
 }
 
+class ItemRepositoryFakeException implements IItemRepository {
+  @override
+  Future<List<Item>> buscarTodosAsync() async {
+    throw Exception();
+    
+  }
+
+  @override
+  List<Item> buscarTodosSync() {
+    
+    return [];
+  }
+
+  @override
+  Item buscarPorId(int id) {
+    return Item(nome: "Item teste ", preco: 10);
+  }
+}
+
+
 void main() {
-  test('Buscar TODOS os Itens Asyncrono...', () async {
+  test('Asyncrono - Buscar TODOS os Itens ...', () async {
     var service = ItemService(repository: ItemRepositoryFake());
 
     final items = await service.buscarTodosAsync();
 
     expect(items, <Item>[]);
+  });
+
+  test('Asyncrono - Buscar TODOS os Itens retornam uma EXCEPTION...',
+      () async {
+    var service = ItemService(repository: ItemRepositoryFakeException());
+
+    final call = service.buscarTodosAsync;
+
+    expect(call(), throwsException);
   });
 }
